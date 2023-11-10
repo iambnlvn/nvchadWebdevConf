323c604e2da20b2372320851e273c8a5abf9d082
iambnlvn <iambnlvn@gmail.com>
2023-11-09 20:00:25 -0600

    Added Tailwind,TS & Golang LSPs...

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project Name
+
+[NvChad](https://github.com/NvChad/NvChad) config for web dev
+
+# Why
+
+Web dev config for NvChad(nvim), including Tailwind css, TS and Golang LSPs,formatters & linters...
+(In case I had to use someone else's machine which prolly uses vs code, I can just clone this repo, plus I'm too lazy to configure, maybe a skill issue tho)
+
+# Usage
+
+Basically, clone this repo and launch nvim
---
+++ lua/custom/mappings.lua
@@ -0,0 +1,25 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+}
+
+-- more keybinds!
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags",
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags",
+    },
+  },
+}
+
+return M
